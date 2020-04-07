0dda44ad7326ae42e91dd4a01fc932a946def5e1
Sosthène <contact@sosthene.net>
2020-04-07 16:47:14 +0200

    TODO: add sha256 check of the downloaded binaries

--- demo/scripts/bitcoin-elements-simple.sh
+++ demo/scripts/bitcoin-elements-simple.sh
@@ -2,6 +2,8 @@
 
 wget -q --no-check-certificate https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/bitcoin-${BITCOIN_VERSION}-x86_64-linux-gnu.tar.gz
 
+#TODO: check sha256 since it seems we can't ssl here
+
 tar -xf bitcoin-${BITCOIN_VERSION}-x86_64-linux-gnu.tar.gz
 
 install -m 0755 -o root -g root -t /usr/local/bin bitcoin-${BITCOIN_VERSION}/bin/*
