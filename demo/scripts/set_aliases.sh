0e252c6e4784b80b363d6b221dea46d23765ecc9
Sosthène <contact@sosthene.net>
2020-04-25 18:08:16 +0200

    add a set_aliases script for easier control of the regtest bitcoin and elements daemon

--- demo/scripts/set_aliases.sh
+++ demo/scripts/set_aliases.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+DIR=/demo/data
 shopt -s expand_aliases
-DIR=/demo
-
-alias b-dae="bitcoind -datadir=$DIR/data/btc"
-alias b-cli="bitcoin-cli -datadir=$DIR/data/btc"
-alias e1-dae="elementsd -datadir=$DIR/data/liquid1"
-alias e1-cli="elements-cli -datadir=$DIR/data/liquid1"
-alias e2-dae="elementsd -datadir=$DIR/data/liquid2"
-alias e2-cli="elements-cli -datadir=$DIR/data/liquid2"
-alias e3-dae="elementsd -datadir=$DIR/data/liquid3"
-alias e3-cli="elements-cli -datadir=$DIR/data/liquid3"
-alias generate1="e1-cli generatetoaddress 1 $(e1-cli getnewaddress)"
-alias generate2="e2-cli generatetoaddress 1 $(e2-cli getnewaddress)"
-alias generate3="e3-cli generatetoaddress 1 $(e3-cli getnewaddress)"
+alias b-dae="bitcoind -datadir=$DIR/btc"
+alias b-cli="bitcoin-cli -datadir=$DIR/btc"
+alias b2-dae="bitcoind -datadir=$DIR/btc2"
+alias b2-cli="bitcoin-cli -datadir=$DIR/btc2"
+alias e1-dae="elementsd -datadir=$DIR/liquid1"
+alias e1-cli="elements-cli -datadir=$DIR/liquid1"
+alias e2-dae="elementsd -datadir=$DIR/liquid2"
+alias e2-cli="elements-cli -datadir=$DIR/liquid2"
+alias e3-dae="elementsd -datadir=$DIR/liquid3"
+alias e3-cli="elements-cli -datadir=$DIR/liquid3"
+#alias generate="e1-cli generatetoaddress 1 $(e1-cli getnewaddress)"
+generate () {
+    local ADDRESS=$(e1-cli getnewaddress)
+    e1-cli generatetoaddress $1 ${ADDRESS}
+}
