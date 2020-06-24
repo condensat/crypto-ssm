3f66f9af4fda87edf93b84a3bff3c0af8fa731e5
Sosthène <contact@sosthene.net>
2020-06-24 21:11:26 +0200

    Add setup.py for the cli

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='crypto-ssm',
+    version='0.1',
+    packages=['cli'],
+    #include_package_data=True,
+    install_requires=['Click==7.0'],
+    entry_points={
+        'console_scripts': [
+            'ssm-cli=cli.cli:cli',
+        ],
+    },
+    description='Simple cli tool for the software secured module',
+    author='Condensat',
+    license='LGPL3',
+)
