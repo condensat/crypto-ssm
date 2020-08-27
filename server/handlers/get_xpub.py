ac098fa35bc636b7bdfac00738a5ca24fe6736e5
Sosthene00 <674694@protonmail.ch>
2020-08-27 19:03:40 +0200

    server: rm useless handlers get_xprv get_xpub restore_master

--- server/handlers/get_xpub.py
+++
@@ -1,9 +0,0 @@
-import api
-import ssm.core as ssm
-
-jsonrpc = api.jsonrpc()
-
-@jsonrpc.method('get_xpub')
-def get_xpub(chain: str, fingerprint: str) -> dict:
-    xpub = ssm.get_xpub(chain, fingerprint)
-    return {'chain': chain, "xpub": xpub}
--- server/handlers/restore_master.py
+++
@@ -1,9 +0,0 @@
-import api
-import ssm.core as ssm
-
-jsonrpc = api.jsonrpc()
-
-@jsonrpc.method('restore_master')
-def restore_master(chain: str, hdkey: str, blindingkey: str) -> dict:
-  fingerprint = ssm.restore_hd_wallet(chain, hdkey, blindingkey)
-  return {'chain': chain, 'fingerprint': fingerprint}
