b624fef0593deb175c27521a31adbff8e0a320d5
Sosthene00 <674694@protonmail.ch>
2020-08-27 19:05:02 +0200

    server: remove handlers from init.py

--- server/handlers/__init__.py
+++ server/handlers/__init__.py
@@ -1,6 +1,3 @@
 from .new_master import new_master
-from .restore_master import restore_master
-from .get_xpub import get_xpub
-from .get_xprv import get_xprv
 from .new_address import new_address
 from .sign_tx import sign_tx
--- server/handlers/new_master.py
+++ server/handlers/new_master.py
@@ -4,6 +4,6 @@ import ssm.core as ssm
 jsonrpc = api.jsonrpc()
 
 @jsonrpc.method('new_master')
-def new_master(chain: str, entropy: str, isbytes: bool) -> dict:
-  fingerprint = ssm.generate_new_hd_wallet(chain, entropy, isbytes)
+def new_master(chain: str, entropy: str, isbytes: bool, size="64") -> dict:
+  fingerprint = ssm.generate_new_hd_wallet(chain, entropy, isbytes, size)
   return {'chain': chain, 'fingerprint': fingerprint}
